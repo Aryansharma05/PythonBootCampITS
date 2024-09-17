5b5544081f575d82404c4197a51ae33835d31283
Aryansharma05 <aryansharma701136@gmail.com>
2024-09-17 13:18:10 +0530

    ubgjvgjc

---
+++ tuppledemo.py
@@ -0,0 +1,11 @@
+#tupple can store multiple data and data cant be changed
+#set do not store duplicate data but tupple do store duplictae
+myTupple = ("aryan" , "sumit" , "yash")
+print(type(myTupple))
+print(myTupple[1])
+#it shows error
+#print values inside tupple
+for name in myTupple:
+ print(name)
+
+    
